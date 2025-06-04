440b7b30fada1c3cb8134ec67cd0398f264b49b3
fang <fang@N100.TinaHome>
2025-06-04 16:26:00 +0800

    community.lua: Add community plugins
    Add astrocommunity.colorscheme.github-nvim-theme
    Add astrocommunity.color.vim-highlighter
    Add astrocommunity.icon.mini-icons
    Add astrocommunity.recipes.heirline-tabline-buffer-number
    
    Signed-off-by: fangying <fangying712@gmaill.com>

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,9 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.color.vim-highlighter" },
+  { import = "astrocommunity.icon.mini-icons" },
+  { import = "astrocommunity.recipes.heirline-tabline-buffer-number" },
   -- import/override with your plugins folder
 }
